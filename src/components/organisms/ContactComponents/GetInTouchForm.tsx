4fa377beebefeb8e3f4ffbad4635e39678e16919
josephjackson0811 <joseph.jackson0811@gmail.com>
2024-07-19 08:14:40 -0400

    change the content for get in touch

--- src/components/organisms/ContactComponents/GetInTouchForm.tsx
+++ src/components/organisms/ContactComponents/GetInTouchForm.tsx
@@ -48,7 +48,7 @@ export default function GetInTouchForm({
       const text = `from: ${data.firstName + ' ' + data.lastName}
                     email: ${data.email}
                     message: ${data.message}`;
-      const res = sendEmail(to, 'Schedule a Demo', text, '');
+      const res = sendEmail(to, 'Get In Touch', text, '');
 
       console.log(res);
 
